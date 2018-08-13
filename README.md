6da19ede73a7aa3484cd73e970dd4e05a0d5d2e5
amao <2207314820@qq.com>
2018-08-13 15:21:43 +0800

    lanyu's npm package

---
+++ README.md
@@ -0,0 +1,10 @@
+## ly 的 npm工具包
+
+## 源码的github地址
+
+## 功能模块 
+    /lib/format     格式化工具
+    /lib/storage    数据存储
+
+## demo
+    存放react和vue的demo
---
+++ index.js
@@ -0,0 +1,15 @@
+import handleStorage from './lib/storage';
+import { floatOperation, stampToDay, getFormatDate, timeFormat, objToStr, listAddKey, moneyFormat } from './lib/format';
+
+export {
+    // 数据存储
+    handleStorage,
+    // 格式化
+    floatOperation, 
+    stampToDay, 
+    getFormatDate, 
+    timeFormat, 
+    objToStr, 
+    listAddKey, 
+    moneyFormat,
+}
