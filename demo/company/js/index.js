9151ff2ce82af783c46ecd4a1b4c48bc5fc15e89
l511407563 <511407563@qq.com>
2018-10-09 00:34:17 +0800

    1

--- demo/company/js/index.js
+++ demo/company/js/index.js
@@ -1,13 +1,8 @@
 window.onload = function () {
-    var tabs = document.querySelectorAll('.menu li'); // 目录
-    var menuTitle = document.getElementsByClassName('menu-title'); // 目录文字
-    var banner = document.getElementById('banner'); // banner
-    var homePage = document.getElementById('homePage'); // 主页内容
-    var otherPages = document.getElementById('otherPages'); // 其他页面
-    var paganation = document.getElementsByClassName('paganation')[0]; // 分页容器
-    var pageBtn = otherPages.querySelectorAll('.page-control'); // 分页按钮
-    var pageLeft = document.querySelector('#arrow_left'); // 分页左按钮
-    var pageRight = document.querySelector('#arrow_right'); // 分页右按钮
+    var tabs = document.querySelectorAll('.menu li'), // 目录
+        menuTitle = document.getElementsByClassName('menu-title'), // 目录文字
+        homePage = document.getElementById('homePage'), // 主页内容
+        otherPages = document.getElementById('otherPages'); // 其他页面
    
     // 目录切换
     tabs.forEach(function (a, i) {
@@ -40,48 +35,82 @@ window.onload = function () {
         });
     });
 
-    // 当前页数索引
-    var _btnIndex = 0;
-    // 分页
-    pageBtn.forEach(function (a, i) {
-        a.addEventListener('click', function () {
-            // 所有分页按钮
-            pageBtn.forEach(function (item) {
-                item.style.backgroundColor = '#fff';
+    // 轮播
+    var bannerTab = document.getElementById("bannerTab"), // 轮播控件
+        bannerLi = bannerTab.getElementsByTagName("li"), // 轮播按钮
+        bannerImg = document.getElementById("bannerMap").getElementsByTagName("img"), // 轮播图片
+        length = bannerLi.length,
+        _bannerIndex = 0;
+
+    for (var i = 0; i < length; i++) {
+        (function(i){
+            bannerLi[i].onclick = function () {
+                if ( _bannerIndex !== i ){
+                    bannerLi[_bannerIndex].classList.remove("on");
+                    bannerImg[_bannerIndex].classList.remove("on");
+                    _bannerIndex = i;
+                    bannerLi[_bannerIndex].classList.add("on");
+                    bannerImg[_bannerIndex].classList.add("on");
+                }
+            };
+        })(i);
+    }
+
+    var paganation = document.getElementsByClassName('paganation')[0], // 分页容器
+        pageBtn = otherPages.querySelectorAll('.page-control'), // 分页按钮
+        pageLeft = document.querySelector('#arrow_left'), // 分页左按钮
+        pageRight = document.querySelector('#arrow_right'), // 分页右按钮
+        _btnIndex = 0;
+
+    (function (_btnIndex) {
+        // 分页
+        pageBtn.forEach(function (a, i) {
+            a.addEventListener('click', function () {
+                // 所有分页按钮
+                pageBtn.forEach(function (item) {
+                    item.style.backgroundColor = '#fff';
+                });
+                this.style.backgroundColor = '#FEB13D';
+                _btnIndex = i;
             });
-            this.style.backgroundColor = '#FEB13D';
         });
-    });
 
-    // 分页左按钮
-    pageLeft.addEventListener('click', function () {
-        pageBtn.forEach(function (a, i) {
-            var c = getComputedStyle(a, null).backgroundColor;
-            if ('rgba(0, 0, 0, 0)' !== c) {
-                if (i === 0) {
-                    _btnIndex--;
-                } else {
-                    _btnIndex = 3;
+        // 分页左按钮
+        pageLeft.addEventListener('click', function () {
+            pageBtn.forEach(function (a, i) {
+                var c = getComputedStyle(a, null).backgroundColor;
+                a.style.backgroundColor = '#fff';
+                if ('rgba(0, 0, 0, 0)' !== c) {
+                    if (_btnIndex === 0) {
+                        _btnIndex = 3;
+                    } else {
+                        console.log(_btnIndex);
+                        _btnIndex--;
+                    }
                 }
-            }
+            });
+            // console.log(_btnIndex, pageBtn[_btnIndex]);
+            pageBtn[_btnIndex].style.backgroundColor = '#FEB13D';
         });
-        console.log(_btnIndex);
-    });
 
-    // 分页右按钮
-    pageRight.addEventListener('click', function () {
-        pageBtn.forEach(function (a, i) {
-            var c = getComputedStyle(a, null).backgroundColor;
-            if ('rgba(0, 0, 0, 0)' !== c) {
-                if (i === 0) {
-                    _btnIndex++;
-                } else {
-                    _btnIndex = 0;
+        // 分页右按钮
+        pageRight.addEventListener('click', function () {
+            pageBtn.forEach(function (a, i) {
+                var c = getComputedStyle(a, null).backgroundColor;
+                a.style.backgroundColor = '#fff';
+                if ('rgba(0, 0, 0, 0)' !== c) {
+                    if (_btnIndex === 3) {
+                        _btnIndex = 0;
+                    } else {
+                        _btnIndex++;
+                    }
                 }
-            }
+            });
+            console.log(_btnIndex);
+            pageBtn[_btnIndex].style.backgroundColor = '#FEB13D';
         });
-        console.log(_btnIndex);
-    });
+    })(_btnIndex)
+    
 
     // 分页切换监听
     function changePage(_btnIndex) {
