46a2caccfefa102a2055e0c016e7a5083f4c67ed
l511407563 <511407563@qq.com>
2018-11-29 13:26:49 +0800

    1

--- demo/html-css-jq-forum/web/js/index.js
+++ demo/html-css-jq-forum/web/js/index.js
@@ -1,34 +1,15 @@
 $(document).ready(function () {
-    // 导航
+    //导航 登陆 用户信息 其他社区跳转区 广告栏 注册 论坛区 讨论区 帖子
+    /* 导航 */
     $('.main-nav li').on('click', function () {
         $(this).siblings('li').removeClass('active');
         $(this).addClass('active');
     });
-    // 导航 end
+    /* 导航 end */
 
-    // 用户信息
 
-    // 用户信息 end
 
-    // 广告栏
-
-    // 广告栏 end
-    
-    // 注册
-    $('.register-btn').on('click', function () {
-        $('.register').show();
-        $('.gg-box').hide();
-        $('.forum').hide();
-        $('.forum-details').hide();
-        $('.article').hide();
-    });
-    // 注册 end
-
-    // 论坛区
-
-    // 论坛区 end
-
-    // 登陆
+    /* 登陆 */
     $('.login-btn').on('click', function () {
         $('.login').hide();
         $('.register').hide();
@@ -41,9 +22,17 @@ $(document).ready(function () {
         $('.login').show();
         $('.userinfo').hide();
     });
-     // 登录 end
+    // 登录 end
 
-    // 其他社区跳转栏
+
+
+    /* 用户信息 */
+
+    /* 用户信息 end */
+
+
+
+    /* 其他社区跳转栏 */
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         slidesPerView: 6,
@@ -51,7 +40,57 @@ $(document).ready(function () {
         spaceBetween: 30,
         freeMode: true
     });
-     // 其他社区跳转栏 end
+    /* 其他社区跳转栏 end */
+
+
+
+    /* 广告栏 */
+
+    /* 广告栏 end */
+    
+
+
+    /* 注册 */
+    $('.register-btn').on('click', function () {
+        $('.register').show();
+        $('.gg-box').hide();
+        $('.forum').hide();
+        $('.forum-details').hide();
+        $('.article').hide();
+    });
+    /* 注册 end */
+
+
+
+    /* 论坛区 */
+
+    /* 论坛区 end */
+
+
+
+    /* 讨论区 */
+    // 分页 
+    laypage({
+        cont: 'pagination', //容器。值支持id名、原生dom对象，jquery对象,
+        pages: 100, //总页数
+        skip: true, //是否开启跳页
+        // skin: 'yahei', //加载内置皮肤，也可以直接赋值16进制颜色值，如:#c00
+        // skin: '#AF0000',
+        // skin: 'molv',
+        skin: '#393A3A',
+        groups: 7 //连续显示分页数
+    });
+    /* 讨论区 end */
+  
+
+
+    /* 帖子 */
+
+    /* 帖子 end */
+
+
+
+    
 
     // 展开/收起
     $('.onOff').on('click', function () {
@@ -110,11 +149,5 @@ $(document).ready(function () {
         $('.article').show();
     });
 
-    // 分页
-    laypage({
-        cont: document.getElementById('page2'), //容器。值支持id名、原生dom对象，jquery对象,
-        pages: 100, //总页数
-        skin: 'yahei', //加载内置皮肤，也可以直接赋值16进制颜色值，如:#c00
-        groups: 7 //连续显示分页数
-    });
+
 })
