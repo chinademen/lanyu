a68dd5ea8275682185650a5784aa0d5192ef61fc
Amao-1 <Amao-1@pbcom20f.ljgroup.co>
2018-12-04 19:03:28 +0800

    1

--- demo/html-css-jq-forum/h5/js/list.js
+++ demo/html-css-jq-forum/h5/js/list.js
@@ -1,3 +1,26 @@
 $(document).ready(function () {
-
+     // 选择页数
+     $('.dumppage').on('change', function () {
+        var page = $(this).children('option:selected').val();
+        $('.currentPage').text('第' + page + '页');
+    });
+    // 上一页
+    $('.prevPage').on('click', function () {
+        var currentPage = $('.dumppage').children('option:selected').val();
+        if (currentPage > 1) {
+            var page = (currentPage - 0 - 1);
+            $('.dumppage').find("option[value = '" + page + "']").attr("selected","selected");
+            $('.currentPage').text('第' + page + '页');
+        }
+    });
+    // 下一页
+    $('.nextPage').on('click', function () {
+        var currentPage = $('.dumppage').children('option:selected').val();
+        var totalPage = $('.dumppage').children('option').length;
+        if (currentPage < totalPage) {
+            var page = parseInt(currentPage) + 1;
+            $('.dumppage').find("option[value = '" + page + "']").attr("selected","selected");
+            $('.currentPage').text('第' + page + '页');
+        }
+    });
 });
