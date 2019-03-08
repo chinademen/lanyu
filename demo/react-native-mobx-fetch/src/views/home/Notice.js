3e50edf2ef54b29f7e8f2320f7feda0600417223
Amao <Amao@LGroup.global>
2019-03-08 18:23:27 +0800

    1

--- demo/react-native-mobx-fetch/src/views/home/Notice.js
+++ demo/react-native-mobx-fetch/src/views/home/Notice.js
@@ -30,29 +30,30 @@ export default class Notice extends PureComponent {
         let { noticeList } = this.props;
         return (
             <View style={styles.container}>
-                    <Image source={require('@/assets/dh/images/home/sound.png')}></Image>
-                    <TouchableOpacity style={styles.scrollBox} onPress={() => {
-                                    if (noticeList && noticeList.length > 0) {
-                                            // alert(noticeList[this.index].title)
-                                            // alert('跳转到公告详情页')
-                                        }
-                                    }} >
-                        <ScrollVertical
-                            onChange={(index => {
-                                this.index = index;
-                            })}
-                            enableAnimation={true}
-                            data={toJS(noticeList)}
-                            delay={2500}
-                            duration={1000}
-                            scrollHeight={34}
-                            scrollStyle={styles.scrollStyle}
-                            textStyle={styles.textStyle}
-                        />
-                    </TouchableOpacity>
-                    <Text style={styles.more} onPress={() => {
-                                        alert('跳转到公告列表页')
-                                    }}>更多>></Text>
+                <Image source={require('@/assets/dh/images/home/sound.png')}></Image>
+                <TouchableOpacity style={styles.scrollBox} onPress={() => {
+                                if (noticeList && noticeList.length > 0) {
+                                        // alert(noticeList[this.index].title)
+                                        // alert('跳转到公告详情页')
+                                    }
+                                }} >
+                    <ScrollVertical
+                        onChange={(index => {
+                            this.index = index;
+                        })}
+                        enableAnimation={true}
+                        data={toJS(noticeList)}
+                        delay={2500}
+                        duration={1000}
+                        scrollHeight={30}
+                        scrollStyle={styles.scrollStyle}
+                        textStyle={styles.textStyle}
+                    />
+                </TouchableOpacity>
+                <View style={styles.more} onPress={() => { alert('跳转到公告列表页') }}>
+                    <Text>更多>></Text>
+                </View>
+                
             </View>
         )
     }
@@ -60,33 +61,37 @@ export default class Notice extends PureComponent {
 
 const styles = StyleSheet.create({
     container: {
-        paddingLeft: 10,
+        height: 30,
+        paddingLeft: gScreen.width * 0.05,
         backgroundColor: '#FFF', 
         width: gScreen.width,
         flexDirection: 'row',
         alignItems: 'center', // 子元素上下居中
     },
     scrollBox: {
-        position: 'relative'
+        height: 30,
     },
     scrollStyle: {
         alignItems: 'flex-start'
     },
     textStyle: {
-        paddingLeft: 5, 
+        paddingLeft: 10, 
         color: '#333', 
         fontSize: 14, 
         backgroundColor: '#fff'
     },
     more: {
-        color: '#333', 
-        padding: 5, 
-        paddingRight: 15, 
+        height: 30,
+        paddingLeft: 5,
+        paddingRight: 15,
+        color: '#333',
+        alignItems:'center',
+        justifyContent: 'center',
+        backgroundColor:'#fff',
         fontSize: 14, 
         position: 'absolute', 
         right: 0, 
         top: 0, 
-        bottom: 0, 
-        backgroundColor: '#fff'
+        bottom: 0,
     }
 })
