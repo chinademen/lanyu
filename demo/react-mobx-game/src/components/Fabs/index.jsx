f89d181b680e12e9c56c8afdd7b0586df0f56fb4
Amao <Amao@LGroup.global>
2019-03-26 18:47:28 +0800

    1

--- demo/react-mobx-game/src/components/Fabs/index.jsx
+++ demo/react-mobx-game/src/components/Fabs/index.jsx
@@ -14,31 +14,58 @@ export default class Fabs extends Component {
     }
 
     // 收起
-    collapse = () => {
+    collapse = (suffix) => {
         this.setState({
-            boxAnimation: 'fabBoxHide',
-            btnAnimation: 'fabBtnShow'
+            boxAnimation: `fabBoxHide_${suffix}`,
+            btnAnimation: `fabBtnShow_${suffix}`
         })
     }
 
     // 展开
-    expand = () => {
+    expand = (suffix) => {
         this.setState({
-            boxAnimation: 'fabBoxShow',
-            btnAnimation: 'fabBtnHide'
+            boxAnimation: `fabBoxShow_${suffix}`,
+            btnAnimation: `fabBtnHide_${suffix}`
         })
     }
 
     render() {
-        const { style, iconStyle, btnStyle } = this.props;
+        /**
+         * @desc    接口参数
+         * @param   {[Object]}  style      [盒子样式]
+         * @param   {[Object]}  iconStyle  [收起按钮样式]
+         * @param   {[Object]}  btnStyle   [展开按钮样式]     
+         * @param   {[String]}  title      [展开按钮文字]
+         * @param   {[String]}  iconhide   [自定义收起icon的class]
+         * @param   {[String]}  iconshow   [自定义展开icon的class]
+         * @param   {[String]}  position   [组件位置]
+        */
+        let { style, iconStyle, btnStyle, title, iconhide, iconshow, position } = this.props;
         const { boxAnimation, btnAnimation } = this.state;
+        iconhide = iconhide || 'icon-side-fold';
+        iconshow = iconshow || 'icon-side-collapse';
+        position = position || 'left';
+        // 组件初始化位置、动画名后缀、icon图标重置
+        let boxPosition = 'cm_left_0';
+        let btnPosition = 'cm_left_36';
+        let suffix = 'left';
+        if (position === 'right') {
+            boxPosition = 'cm_right_0';
+            btnPosition = 'cm_right_36';
+            suffix = 'right';
+            iconshow = 'icon-side-fold';
+            iconhide = 'icon-side-collapse';
+        }
 
         return (
             <Fragment>
-                <div className={`cm-fabs-box ${boxAnimation}`} style={style || null}>
-                    <span className="cm-fabs-icon" onClick={this.collapse} style={[iconStyle || null]}>{'<'}</span>
+                <div className={`cm-fabs-box ${boxAnimation} ${boxPosition}`} style={style || null}>
+                    <span className={`cm-fabs-icon ${iconhide}`} onClick={() => this.collapse(suffix)} style={[iconStyle || null]}></span>
                 </div>
-                <div className={`cm-fabs-btn ${btnAnimation}`} onClick={this.expand} style={btnStyle || null}></div>
+                <div className={`cm-fabs-btn ${btnAnimation} ${btnPosition}`} onClick={() => this.expand(suffix)} style={btnStyle || null}>
+                    <span className={`cm-task-title`}>{title || `新手大礼`}</span>
+                    <i className={`${iconshow}`}></i>
+                </div>
             </Fragment>
         )
     }
--- demo/react-mobx-game/src/views/Home/index.js
+++ demo/react-mobx-game/src/views/Home/index.js
@@ -2,33 +2,6 @@ import React, { Component } from 'react';
 import _ from 'lodash';
 import { inject, observer } from 'mobx-react';
 import Header from '@/components/Header';
-import Fabs from '@/components/Fabs';
-import Msg from '@/components/Msg';
-// import BasicCarousel from '@/components/BasicCarousel';
-import img0 from '@/assets/images/carousel/img0.png';
-import img1 from '@/assets/images/carousel/img1.png';
-import img2 from '@/assets/images/carousel/img2.png';
-import img3 from '@/assets/images/carousel/img3.png';
-import img4 from '@/assets/images/carousel/img4.png';
-import img5 from '@/assets/images/carousel/img5.png';
-
-const imgArr = [
-    { src: img0, fn: () => window.open('http://www.baidu.com') },
-    { src: img1, fn: () => window.open('http://www.baidu.com') },
-    { src: img2, fn: () => window.open('http://www.baidu.com') },
-    { src: img3, fn: () => window.open('http://www.baidu.com') },
-    { src: img4, fn: () => window.open('http://www.baidu.com') },
-    { src: img5, fn: () => window.open('http://www.baidu.com') },
-];
-
-// console.log(lodash.chunk);
-var users = [
-    { 'user': 'barney',  'active': true },
-    { 'user': 'fred' },
-    { 'user': 'pebbles', 'active': false }
-];
-// console.log(_.dropRightWhile(users, 'active'))
-// => objects for ['barney']
 
 @inject('commonStore', 'homeStore', 'lotteryStore')
 @observer
@@ -36,22 +9,12 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            num: 0, // 消息号
-            msgInfo: null, // 消息实体
+       
         };
     }
 
     componentDidMount() {
-        let { num } = this.state;
-        setInterval(() => {
-            if (num < 15) {
-                num++;
-                this.setState({
-                    num: num,
-                    msgInfo: `新消息${num}`
-                });
-            }
-        }, 3000);
+      
     }
 
     render() {
@@ -59,20 +22,6 @@ class Home extends Component {
         return (
             <div>
                 <Header />
-                <Fabs 
-                    style={{ 
-                        backgroundImage: `url(${require('@/assets/images/carousel/img0.png')})`
-                        
-                    }}
-                />
-                {/* <BasicCarousel
-                    list={imgArr}
-                    control={true}
-                    width={600}
-                    height={600}
-                    showItems={3}
-                /> */}
-                {/* <Msg msgInfo={this.state.msgInfo} /> */}
             </div>
         )
     }
