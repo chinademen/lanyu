897a7f0f6b4b280d85913005b1e971a038c65301
l511407563 <511407563@qq.com>
2019-03-30 00:39:33 +0800

    1

--- demo/react-mobx-game/src/components/Hoc/index.js
+++ demo/react-mobx-game/src/components/Hoc/index.js
@@ -1,14 +1,30 @@
 /* 高阶组件, 可用于动态生成组件 */
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from 'react'
+import throttle from 'lodash.throttle'
 
 const HightComponent = (MiddeleComponent) => {
     return class extends Component {
+        constructor(props) {
+            super(props);
+            this.onClick = this.onClick.bind(this);
+            this.onClickThrottle = throttle(this.onClick, 3000);
+        }
+
+        // 节流
+        onClick = (event) => {
+            event()
+        }
+
         render() {
             return (
                 <Fragment>
-                    <MiddeleComponent {...this.props} />
+                    <MiddeleComponent {...this.props} onClickThrottle={this.onClickThrottle} />
                 </Fragment>
             )
+        }
+
+        componentWillUnmount() {
+            this.onClickThrottle.cancel();
         }
     }
 };
